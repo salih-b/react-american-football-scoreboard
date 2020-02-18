3669125066065d2d8a9e4dca02fd1ac65810ed6f
Salih Babekr <salihmorta17@gmail.com>
2020-02-18 10:48:27 -0500

    ...

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
+
+
 function App() {
   //TODO: STEP 2 - Establish your applictaion's 
   // state with some useState hooks.  You'll need one for 
   // the home score and another for the away score.
+  // const TigerHandler = function (increase){
+  //   setTscore(Tscore + increase)
+  // };
   const [Lscore, setLscore] = useState(0);
   const [Tscore, setTscore] = useState(0);
   return (
